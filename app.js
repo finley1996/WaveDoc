6661435110a658261157c54f58eba88fc8e17495
徐敏航 <minhangxu@shein.com>
2023-04-25 18:04:47 +0800

    add dockerfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 8009;
 
 const DEFAULT_PAGE_SIZE = 10;
 const MAX_PAGE_SIZE = 20;
